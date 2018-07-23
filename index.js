464f452dec3cce13bf1324c1a79816ea9dbe2c37
Marissa Zhong <mwz2101@columbia.edu>
2018-07-23 11:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ function init() {
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+function createRecipe() {
+  let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
+  let result = template(recipe);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
+
+Handlebars.displayIngredient('ingredient', function() {
+
+})
+
+function displayEditForm() {
+
+}
+
+function updateRecipe() {
+
+}
