5d1b1e9500118d7a55e2dcd0705b53446e7fc8c8
Marissa Zhong <mwz2101@columbia.edu>
2018-07-23 11:52:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-function init() {
-  //put any page initialization/handlebars initialization here
-}
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
 
 function createRecipe() {
   let template = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
   let result = template(recipe);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
-
-Handlebars.displayIngredient('ingredient', function() {
-
-})
 
 function displayEditForm() {
 
@@ -22,3 +12,19 @@ function displayEditForm() {
 function updateRecipe() {
 
 }
+
+function init() {
+  //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper('displayIngredient', function(ingredient) {
+    return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+  })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+document.addEventListener("DOMContentLoaded", function(event) {
+  init()
+})
